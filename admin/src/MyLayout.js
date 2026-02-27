a3f84675a23ffeaebe2b41157a1fc21d5ab0adcd
Viktor Nareiko <viktor.nareiko@ibm.com>
2026-02-27 10:25:29 +0200

    Remove unrelevant files

--- admin/src/MyLayout.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { Layout } from 'react-admin';
-import MyMenu from './MyMenu';
-
-const MyLayout = (props) => <Layout {...props} menu={MyMenu}/>;
-
-export default MyLayout;
