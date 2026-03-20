12d2be414fa32b7caa0cbfd802e8cee8c5d43fe3
Viktor Nareiko <viktor.nareiko@ibm.com>
2026-03-20 16:38:24 +0200

    Introducing sass, removing tailwind, fixed HMR websocket error

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -2,12 +2,17 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 const outDir = process.env.BUILD_OUT_DIR || 'dist';
+const devPort = Number(process.env.BUMBLEBEE_FRONTEND_DEV_PORT || 5173);
 
 export default defineConfig({
   plugins: [vue()],
   server: {
     host: true,
-    port: 5173
+    port: devPort,
+    strictPort: true,
+    hmr: {
+      clientPort: devPort
+    }
   },
   build: {
     outDir
